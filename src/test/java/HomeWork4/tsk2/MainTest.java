74bb387a20328397392b18b22f12b068b7a19413
jekoGetMan <sl1mvsshady@gmail.com>
2018-12-02 22:48:54 +0200

    Test methods

--- src/test/java/HomeWork4/tsk2/MainTest.java
+++ src/test/java/HomeWork4/tsk2/MainTest.java
@@ -1,17 +1,21 @@
 package HomeWork4.tsk2;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+import org.junit.Test;
+import junit.framework.Assert.*;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.verify;
+
 
 public class MainTest {
-
+    Planet p1 = new Planet("Venus");
+    Island island = new Island("Hawaii");
     @Test
     public void main() {
-
-    }
-
-    @Test
-    public void displayFirstContinentName() {
+        p1.newOcean(new Ocean("Arctic Ocean"));
+        p1.newOcean(new Ocean("Indian Ocean"));
+        Assert.assertEquals("My name is:Heisenberg",p1.newContinent(new Continent("Australia")));
+        Assert.assertEquals(2,pl.displayFirstContinentName());
+        Assert.assertEquals(3,pl.displayFirstContinentName());
     }
 }
